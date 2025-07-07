f3fea1e99f846a8c08ce47660ce12c353131d32c
Greeceuniquecode <notgreece2@gmail.com>
2025-07-07 16:56:16 +0545

    Route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./landingPage/LandingPage";
-import TestPage from "./user/UserDetailsPage";
 import Layouts from "./layouts/Layouts";
 import UserIdForm from "./user/UserIdForm";
 import UserDetailsPage from "./user/UserDetailsPage";
@@ -12,7 +11,7 @@ const App = () => {
         <Route path="/" element={<Layouts />} >
         <Route index element={<LandingPage />} />
           <Route path="/nagarikta" element={<UserIdForm />} />
-          <Route path="/test" element={<UserDetailsPage />} />
+          <Route path="/user-details" element={<UserDetailsPage />} />
           {/* Dummy routes for other navbar links */}
           <Route path="/home" element={<div className="p-10">परिचय पृष्ठ</div>} />
           <Route path="/about-us" element={<div className="p-10">हाम्रो बारेमा</div>} />
